b37183efd46e0c0a9adecf2eb008797baaa56276
Bethany Albright <balbright0186@gmail.com>
2023-03-30 07:03:11 -0400

    myfile.md

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  if(400 >= value){
+    return "This one is on me!";
+  } else if (1500 >= value){
+    return "That will be twenty bucks.";
+  
+  } else if (2001 >= value){
+      return "I will gladly take your thirty bucks.";
+  } else if (2501 >= value){
+      return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  if (city ==='NYC'){
+    return 'Ok, sounds good.';
+  }
+  else if (city !== 'NYC'){
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+  case "generous": 
+    return("Thank you so much.");
+  case "not as generous":
+    return("Thank you.");
+  default:"if anything else"
+    return("Bye.");
+  
+  }
 }
